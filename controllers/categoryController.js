d3fddc2a64e1247d5b905b906b8723cf85ac13ef
Mateolima25 <mateolima2017@gmail.com>
2025-04-02 16:04:07 -0300

    cate

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,13 +3,10 @@ const { Category } = require("../models");
 async function getCategories(req, res) {
   try {
     const categories = await Category.findAll();
-    console.log("Categories fetched:", categories);
-
     if (categories.length === 0) {
       return res.status(404).json({ message: "No categories found" });
     }
-
-    return res.json({ categories: categories.map((cat) => cat.toJSON()) });
+    return res.json({ categories });
   } catch (error) {
     console.error("Error fetching categories:", error);
     return res.status(500).json({ error: "Error fetching categories" });
