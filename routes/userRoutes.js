47a02ec9325d53308b38a0c63e17f444a8ad0ac3
FelipeZocalo <felipezoquii@gmail.com>
2025-04-09 11:08:12 -0300

    RUTAS DE USER PROTEGIDAS

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
+router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
 router.get("/", userController.index);
 router.get("/:id", userController.show);
 router.put("/:id", userController.update);
-
 router.delete("/:id", userController.deleteUser);
-router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
 
 module.exports = router;
