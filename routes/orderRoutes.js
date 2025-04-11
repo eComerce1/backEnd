fbbf60b85222ab3d47391b12fa431cf6793de718
FelipeZocalo <felipezoquii@gmail.com>
2025-04-10 21:22:24 -0300

    problemas con las rutas solucionados!

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -16,14 +16,11 @@ const { expressjwt: checkJwt } = require("express-jwt");
 
 const router = express.Router();
 
+router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
 router.put("/:orderId/status", updateOrderStatus);
 
 router.get("/last-month", getOrdersLastMonth);
 router.get("/", getOrders);
-/*ACÁ*/
-router.get("/:orderId", getOrderWithProducts);
-/*ACÁ*/
-router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
 
 router.get("/cart", getCart);
 router.post("/cart", addToCart);
@@ -31,5 +28,6 @@ router.delete("/cart/:productId", removeFromCart);
 router.post("/update-address", updateOrderAddress);
 router.delete("/clear", clearCart);
 router.get("/userOrders", getUserOrders);
+router.get("/:orderId", getOrderWithProducts);
 
 module.exports = router;
